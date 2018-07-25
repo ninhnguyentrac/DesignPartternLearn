8f3cd7a0ecdee8efa79e114967a260586373e354
NITECO\ninh.nguyen <ninh.nguyen@niteco.se>
2018-07-25 11:03:31 +0700

    First commit

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    class Adapter : ITarget
+    {
+      private Adaptee _adaptee = new Adaptee();
+    
+      public void Request()
+      {
+        _adaptee.SpecificRequest();
+      }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Adapter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ITarget target = new Adapter();
+      target.Request();
+      Console.ReadKey();
+    }
+  }
+}
